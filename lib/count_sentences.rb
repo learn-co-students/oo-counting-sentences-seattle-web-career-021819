f53d2e0d084adb5cabcea5379c5039c1f106bb4d
github username <github email address>
2019-02-20 18:07:29 -0800

    oo counting lab complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+   self.end_with?("!")
   end
 
   def count_sentences
+    array = self.split(/[.!?]/)
 
+
+      output = array.delete_if do |element|
+        element == ""
+      end
+
+    output.length
+
+
+    #binding.pry
+    #return output_array.length
   end
+
 end
